f37bb35e4d5c53d4a0dcfff359a2d7dcc5526bdb
Jianbo Jia <jianbojia@Jianbos-iMac.local>
2020-03-04 16:50:33 -0800

    Complete Charpter 17: Files

---
+++ src/main/java/charpter16validating/StringInverter.java
@@ -0,0 +1,9 @@
+// validating/StringInverter.java
+// (c)2017 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+package charpter16validating;
+
+public interface StringInverter {
+  String invert(String str);
+}
